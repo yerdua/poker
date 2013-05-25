dd9138c6b7bbd6737b98990b268778adefdc14f9
App Academy Student <student@appacademy.io>
2013-05-24 17:03:47 -0700

    Added specs and behavior for the deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,2 +1,43 @@
+require 'card'
+
 class Deck
+
+  attr_reader :cards
+
+  def initialize(cards = nil)
+    @cards = cards || create_cards
+  end
+
+  def count
+    @cards.count
+  end
+
+  def shuffle!
+    @cards.shuffle!
+  end
+
+  def deal(number = 1)
+    if number > count
+      raise OutOfCardsError.new("Can't deal more cards than exist in the deck")
+    end
+    deal = []
+    number.times { deal << @cards.shift }
+    deal
+  end
+
+  private
+
+    def create_cards
+      cards = []
+      @cards = Card::SUITS.each_key do |suit|
+        (1..13).each do |value|
+          cards << Card.new(value, suit)
+        end
+      end
+      cards
+    end
+
+end
+
+class OutOfCardsError < ArgumentError
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,4 +2,42 @@ require 'rspec'
 require 'deck'
 
 describe "Deck" do
+  subject(:deck) { Deck.new }
+
+  it "has 52 cards" do
+    subject.count.should == 52
+  end
+
+  describe "#shuffle!" do
+    it "changes the order of the cards" do
+      original_order = subject.cards.dup
+      subject.shuffle!
+      subject.cards.should_not == original_order
+    end
+  end
+
+  describe "#deal" do
+    it "returns the specified number of cards" do
+      subject.deal(5).count == 5
+    end
+
+    it "deals one card by default" do
+      subject.deal.count == 1
+    end
+
+    it "decreases the number of cards in the deck" do
+      original_count = subject.count
+      subject.deal(5)
+      subject.count.should == original_count - 5
+    end
+
+    it "removes dealt cards from the deck" do
+      card = subject.deal(1).first
+      subject.cards.should_not include(card)
+    end
+
+    it "raises an error if the number requested exceeds what's left" do
+      expect { subject.deal(60) }.to raise_error(OutOfCardsError)
+    end
+  end
 end
